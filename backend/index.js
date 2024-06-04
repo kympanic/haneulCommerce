403b53143703d39ba5b5525393f93732ccc8703e
Daniel Yoo <dacapo0509@gmail.com>
2024-06-04 16:03:27 -0700

    successfully created asynchandler middleware, usercontroller,usermodel,and user route for creating a user. Successfully tested user in postman

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,9 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
 //Utilities
 import connectDB from "./config/db.js";
-
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 8000;
 
@@ -16,8 +15,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-	res.send("hello world");
-});
-
+app.use("/api/users", userRoutes);
 app.listen(port, () => console.log(`Server running on port ${port}`));
