c7ed50593ed629e619c5232c6f948257cfea90f2
Daniel Yoo <dacapo0509@gmail.com>
2024-06-04 16:20:06 -0700

    creating user using jwt token for saving to cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
+import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
 	const { username, email, password } = req.body;
@@ -14,9 +16,13 @@ const createUser = asyncHandler(async (req, res) => {
 	const userExists = await User.findOne({ email });
 	if (userExists) res.status(400).send("User already exists");
 
-	const newUser = new User({ username, email, password });
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	const newUser = new User({ username, email, password: hashedPassword });
 	try {
 		await newUser.save(); // save new user to db
+		createToken(res, newUser._id);
 
 		res.status(201).json({
 			_id: newUser._id,
